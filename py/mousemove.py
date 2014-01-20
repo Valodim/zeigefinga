d0fd4b5d23e0e0ad8a815a900a5e7e0f932f9d1a
Vincent Breitmoser <valodim@mugenguild.com>
2014-01-20 13:25:47 +0100

    absolute input from a mouse does not work that way

--- py/mousemove.py
+++ py/mousemove.py
@@ -11,7 +11,7 @@ import lib.bufs as bufs
 from lib.streaminput import StreamInput
 
 # remap input fields?
-fields = map(lambda x: int(x)-1, sys.argv[1:]) if len(sys.argv) > 1 else None
+fields = [ int(x)-1 for x in sys.argv[1:] ] if len(sys.argv) > 1 else None 
 # get a stream input object from sys.stdin
 stream = StreamInput(sys.stdin, fields=fields)
 # prototype from three consecutive lines of equal shape
@@ -22,8 +22,6 @@ gen = iter(stream)
 device = uinput.Device([
     uinput.REL_X,
     uinput.REL_Y,
-    uinput.ABS_X + (0, 255, 0, 0),
-    uinput.ABS_Y + (0, 255, 0, 0),
     uinput.BTN_LEFT,
     uinput.BTN_RIGHT
     ])
@@ -38,11 +36,7 @@ def update_line(*args):
         sys.exit(0)
 
     print int(inp[1]), int(inp[0])
-    if False: # or int(cbuf.get()[-1,6]) is 1:
-        print "!"
-        device.emit(uinput.ABS_X, 127)
-        device.emit(uinput.ABS_Y, 127)
-    else:
+    if len(inp) <= 2 or inp[2] == 1:
         device.emit(uinput.REL_X, int(inp[0]), syn=False)
         device.emit(uinput.REL_Y, -int(0.8*inp[1]))
 
