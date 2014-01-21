a88debb0d8dd703a8ea9f40fb64e0293a971a5fa
Vincent Breitmoser <valodim@mugenguild.com>
2014-01-21 02:29:11 +0100

    lufa + contiki, first steps
    use contiki as a base and integrate lufa as a library. requires two
    patches to get rid of all usb and cfs stuff in contiki for avr-ravenusb:
    
    24a7ab5b24daf15452b6a22ba1d6bbe03d4ee680
    1e1adbb63e5573a59440bd912ce09f36a9a4c1e6
    
    rough usage notes:
    make libzeigefinga.a  # compile static lib with lufa stuff
    make zeigefinga       # compile contiki binary (with lib linked in)
    make avrdude          # upload with jtag2
    
    ENTIRELY UNTESTED because I didn't have a programmer available!

---
+++ lufa/zeigefinga.c
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2013, TU Braunschweig
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * @file zeigefinga.c
+ */
+
+#include <stdio.h>
+#include "contiki.h"
+#include "Config/LUFAConfig.h"
+#include "lufa.h"
+
+/** Buffer to hold the previously generated Mouse HID report, for comparison purposes inside the HID class driver. */
+static uint8_t PrevMouseHIDReportBuffer[sizeof(USB_MouseReport_Data_t)];
+
+/** LUFA HID Class driver interface configuration and state information. This structure is
+ *  passed to all HID Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_HID_Device_t Mouse_HID_Interface =
+	{
+		.Config =
+			{
+				.InterfaceNumber              = INTERFACE_ID_Mouse,
+				.ReportINEndpoint             =
+					{
+						.Address              = MOUSE_EPADDR,
+						.Size                 = MOUSE_EPSIZE,
+						.Banks                = 1,
+					},
+				.PrevReportINBuffer           = PrevMouseHIDReportBuffer,
+				.PrevReportINBufferSize       = sizeof(PrevMouseHIDReportBuffer),
+			},
+	};
+
+/*---------------------------------------------------------------------------*/
+PROCESS(radio_process, "radio input process");
+PROCESS(lufa_process, "Accelerometer process");
+AUTOSTART_PROCESSES(&radio_process, &lufa_process);
+/*---------------------------------------------------------------------------*/
+static struct etimer timer;
+PROCESS_THREAD(radio_process, ev, data)
+{
+    PROCESS_BEGIN();
+
+    // just wait shortly to be sure sensor is available
+    etimer_set(&timer, CLOCK_SECOND * 0.05);
+    PROCESS_YIELD();
+
+    while (1) {
+        if(ev == PROCESS_EVENT_TIMER) {
+            // TODO: implement input from radio
+            printf("hi\n");
+            etimer_reset(&timer);
+
+        }
+        PROCESS_YIELD();
+        // PROCESS_PAUSE();
+    }
+
+    PROCESS_END();
+}
+
+PROCESS_THREAD(lufa_process, ev, data)
+{
+    PROCESS_BEGIN();
+
+    /* Disable clock division */
+    // not sure if this is needed?
+    // clock_prescale_set(clock_div_1);
+
+    /* Hardware Initialization */
+    // Joystick_Init();
+    // LEDs_Init();
+    // Buttons_Init();
+    USB_Init();
+
+    while (1) {
+        HID_Device_USBTask(&Mouse_HID_Interface);
+        USB_USBTask();
+        PROCESS_PAUSE();
+    }
+
+    PROCESS_END();
+
+
+}
+
+/** Event handler for the library USB Connection event. */
+void EVENT_USB_Device_Connect(void)
+{
+	LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+}
+
+/** Event handler for the library USB Disconnection event. */
+void EVENT_USB_Device_Disconnect(void)
+{
+	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+}
+
+/** Event handler for the library USB Configuration Changed event. */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+	bool ConfigSuccess = true;
+
+	ConfigSuccess &= HID_Device_ConfigureEndpoints(&Mouse_HID_Interface);
+
+	USB_Device_EnableSOFEvents();
+
+	LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
+}
+
+/** Event handler for the library USB Control Request reception event. */
+void EVENT_USB_Device_ControlRequest(void)
+{
+	HID_Device_ProcessControlRequest(&Mouse_HID_Interface);
+}
+
+/** Event handler for the USB device Start Of Frame event. */
+void EVENT_USB_Device_StartOfFrame(void)
+{
+	HID_Device_MillisecondElapsed(&Mouse_HID_Interface);
+}
+
+/** HID class driver callback function for the creation of HID reports to the host.
+ *
+ *  \param[in]     HIDInterfaceInfo  Pointer to the HID class interface configuration structure being referenced
+ *  \param[in,out] ReportID    Report ID requested by the host if non-zero, otherwise callback should set to the generated report ID
+ *  \param[in]     ReportType  Type of the report to create, either HID_REPORT_ITEM_In or HID_REPORT_ITEM_Feature
+ *  \param[out]    ReportData  Pointer to a buffer where the created report should be stored
+ *  \param[out]    ReportSize  Number of bytes written in the report (or zero if no report is to be sent)
+ *
+ *  \return Boolean \c true to force the sending of the report, \c false to let the library determine if it needs to be sent
+ */
+bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
+                                         uint8_t* const ReportID,
+                                         const uint8_t ReportType,
+                                         void* ReportData,
+                                         uint16_t* const ReportSize)
+{
+    USB_MouseReport_Data_t* MouseReport = (USB_MouseReport_Data_t*)ReportData;
+
+    // stupid dummy, just move the mouse around
+    MouseReport->X = -1;
+    MouseReport->Y =  1;
+
+    *ReportSize = sizeof(USB_MouseReport_Data_t);
+    return true;
+}
+
+/** HID class driver callback function for the processing of HID reports from the host.
+ *
+ *  \param[in] HIDInterfaceInfo  Pointer to the HID class interface configuration structure being referenced
+ *  \param[in] ReportID    Report ID of the received report from the host
+ *  \param[in] ReportType  The type of report that the host has sent, either HID_REPORT_ITEM_Out or HID_REPORT_ITEM_Feature
+ *  \param[in] ReportData  Pointer to a buffer where the received report has been stored
+ *  \param[in] ReportSize  Size in bytes of the received HID report
+ */
+void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
+        const uint8_t ReportID,
+        const uint8_t ReportType,
+        const void* ReportData,
+        const uint16_t ReportSize)
+{
+    // Unused (but mandatory for the HID class driver) in this demo, since there are no Host->Device reports
+}
+
