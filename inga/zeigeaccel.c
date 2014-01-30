d743c387a869a119eef943f023018bddfc59cc19
Vincent Breitmoser <valodim@mugenguild.com>
2014-01-30 16:03:18 +0100

    inga: zeigeaccel.c, second attempt

--- inga/zeigeaccel.c
+++ inga/zeigeaccel.c
@@ -29,7 +29,7 @@
  */
 
 /**
- * @file gyro-direct-fifo.c
+ * @file zeigeaccel.c
  * @author Vincent Breitmoser <v.breitmoser@tu-bs.de>
  */
 
@@ -39,7 +39,9 @@
 #include "acc-sensor.h"
 #include "button-sensor.h"
 #include "button-sensor2.h"
-#include "l3g4200d.h"
+
+#include "keycodes.h"
+
 typedef struct {
     int8_t x, y;
     uint8_t button;
@@ -49,13 +51,13 @@ typedef struct {
 static buf_xy_t buf_xy;
 
 /*---------------------------------------------------------------------------*/
-PROCESS(gyro_process, "gyro process");
-AUTOSTART_PROCESSES(&gyro_process);
+PROCESS(accel_process, "accel process");
+AUTOSTART_PROCESSES(&accel_process);
 /*---------------------------------------------------------------------------*/
 static struct etimer timer;
 static const struct broadcast_callbacks broadcast_call = {};
 static struct broadcast_conn broadcast;
-PROCESS_THREAD(gyro_process, ev, data)
+PROCESS_THREAD(accel_process, ev, data)
 {
 
     PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
@@ -63,7 +65,6 @@ PROCESS_THREAD(gyro_process, ev, data)
     PROCESS_BEGIN();
 
     SENSORS_ACTIVATE(button_sensor);
-    SENSORS_ACTIVATE(button_sensor2);
 
     broadcast_open(&broadcast, 129, &broadcast_call);
 
@@ -71,36 +72,10 @@ PROCESS_THREAD(gyro_process, ev, data)
     etimer_set(&timer, CLOCK_SECOND * 0.05);
     PROCESS_YIELD();
 
-    // doing gyro initialization directly because fuck contiki
-    if (l3g4200d_init() != 0) {
-        printf("Error: Failed to init gyro / step 1, aborting...\n");
-        PROCESS_EXIT();
-    }
-
-    // L3G4200D_250DPS // L3G4200D_500DPS // L3G4200D_2000DPS
-    if(l3g4200d_set_dps(L3G4200D_250DPS) != 0) {
-        printf("Error: Failed to init gyro / step 2, aborting...\n");
-        PROCESS_EXIT();
-    }
-
-    // L3G4200D_ODR_100HZ // L3G4200D_ODR_200HZ // L3G4200D_ODR_400HZ // L3G4200D_ODR_800HZ
-    if(l3g4200d_set_data_rate(L3G4200D_ODR_400HZ) != 0) {
-        printf("Error: Failed to init gyro / step 3, aborting...\n");
-        PROCESS_EXIT();
-    }
-
-    // L3G4200D_BYPASS - no fifo, just the single latest value
-    // L3G4200D_STREAM - just keep streaming
-    // L3G4200D_FIFO - break on fifo overrun
-    // l3g4200d_set_fifomode(L3G4200D_BYPASS);
-    l3g4200d_set_fifomode(L3G4200D_STREAM);
-    l3g4200d_fifo_enable();
-
     // get pointer to sensor
-    static const struct sensors_sensor *acc_sensor;
-    acc_sensor = sensors_find("Acc");
+    static const struct sensors_sensor *acc_sensor; {
+        acc_sensor = sensors_find("Acc");
 
-    {
         // activate and check status
         uint8_t status = SENSORS_ACTIVATE(*acc_sensor);
         if (status == 0) {
@@ -116,95 +91,66 @@ PROCESS_THREAD(gyro_process, ev, data)
     // how long to wait for the fifo to fill
     etimer_set(&timer, CLOCK_SECOND * 0.03);
 
-    int i, num;
-    int16_t x, y, z;
-    angle_data_t gyro_values[L3G4200D_FIFO_SIZE];
-
-    static uint8_t state = 0;
-    static struct {
-        uint8_t num;
-        int32_t x;
-        int32_t y;
-    } acc_state;
-    acc_state.x = acc_state.y = 0;
-    int16_t trust = 0;
-
     while (1) {
         PROCESS_YIELD();
 
         if(ev == PROCESS_EVENT_TIMER && data == &timer) {
-            x = y = z = 0;
 
-            // get fifo values
-            num = l3g4200d_get_angle_fifo(gyro_values);
-            // or emulate fifo data from bypass mode
-            // gyro_values[0] = l3g4200d_get_angle(); num = 1;
+            static uint8_t state = 0;
+            static struct {
+                int16_t x;
+                int16_t y;
+            } acc_state;
 
-#if 1
-            // approach 1: accumulate and divide later
-            // smoother movement, lack of a deadzone leads to shaky cursor
-            for(i = 0; i < num; i++) {
-                x += (gyro_values[i].x * 0.00875);
-                y += (gyro_values[i].y * 0.00875);
-            }
-            x /= num; y /= num; z /= num;
-#else
-            // approach 2: divide individual values
-            // works as a natural deadzone, which makes controls feel less but
-            // allows keeping the cursor in position easily
-            for(i = 0; i < num; i++) {
-                x += (gyro_values[i].x * 0.00875) / num;
-                y += (gyro_values[i].y * 0.00875) / num;
-                z += (gyro_values[i].z * 0.00875) / num;
-            }
-#endif
             uint8_t key = 0;
             switch(state) {
                 case 0:
                     if(!button_sensor.value(0))
                         break;
                     state = 1;
-                    acc_state.num = 0;
+                    acc_state.x = acc_sensor->value(ACC_X);
+                    acc_state.y = acc_sensor->value(ACC_Y);
                 case 1:
-                    trust = acc_sensor->value(ACC_Z);
-                    printf("%d\t%d\t%d\n", acc_sensor->value(ACC_X), acc_sensor->value(ACC_Y), trust);
-                    // roughly, trust the X and Y values if Z is > 0
-                    if(trust < -700) {
-                        acc_state.x += acc_sensor->value(ACC_X);
-                        acc_state.y += acc_sensor->value(ACC_Y);
-                        acc_state.num += 1;
-                    }
+                    printf("%d\t%d\n", acc_sensor->value(ACC_X), acc_sensor->value(ACC_Y));
+
                     // still pressed down?
                     if(button_sensor.value(0))
                         break;
 
-                    acc_state.x /= acc_state.num;
-                    acc_state.y /= acc_state.num;
+                    acc_state.x -= acc_sensor->value(ACC_X);
+                    acc_state.y -= acc_sensor->value(ACC_Y);
+
+                    if(acc_state.x < -1000) {
+                        printf("left\n");
+                        key = HID_KEYBOARD_SC_LEFT_ARROW;
+                    } else if(acc_state.x > 1000) {
+                        printf("right\n");
+                        key = HID_KEYBOARD_SC_RIGHT_ARROW;
+                    } else if(acc_state.y < -700) {
+                        printf("forward\n");
+                        key = HID_KEYBOARD_SC_UP_ARROW;
+                    } else if(acc_state.y > 700) {
+                        printf("back\n");
+                        key = HID_KEYBOARD_SC_DOWN_ARROW;
+                    }
 
                     // check for gestures
-                    printf("> %ld\t%ld\t%d\n", acc_state.x, acc_state.y, acc_state.num);
+                    printf("> %d\t%d\n", acc_state.x, acc_state.y);
 
                     // reset all values
                     acc_state.x = acc_state.y = 0;
                     state = 0;
             }
 
+            // if any key fell out of that stuff above, send it to the raven
             if(key) {
                 buf_xy.x = buf_xy.y = 0;
+                buf_xy.modifier = 0;
                 buf_xy.key = key;
                 packetbuf_copyfrom(&buf_xy, sizeof(buf_xy_t));
                 broadcast_send(&broadcast);
             }
 
-#if 0
-            if(button_sensor.value(0)) {
-                buf_xy.x = z;
-                buf_xy.y = -y;
-                buf_xy.button = button_sensor2.value(1);
-                packetbuf_copyfrom(&buf_xy, sizeof(buf_xy_t));
-                broadcast_send(&broadcast);
-            }
-#endif
             // reset timer
             etimer_reset(&timer);
         }
